68c6944a3f375f1062bc575ae251daf37e0c146b
webQbe <webcubesolutionslk@gmail.com>
2025-09-02 21:15:59 +0530

    Implemented middleware to run on every request & attach .company to it based on logged-in user

--- ac_project/ac_project/settings.py
+++ ac_project/ac_project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accounts_core.middleware.CurrentCompanyMiddleware', # Attach .company to request based on logged-in user
 ]
 
 ROOT_URLCONF = 'ac_project.urls'
--- ac_project/accounts_core/managers.py
+++ ac_project/accounts_core/managers.py
@@ -4,11 +4,17 @@ from django.db import models
 # that belong to a company 
 # -----------------------------------------
 # Define subclass of Django’s QuerySet
-class TenantQuerySet(models.QuerySet):      
-    def for_company(self, company):         # Add helper
+class TenantQuerySet(models.QuerySet):  
+    def for_company(self, company):         # Add queryset helper
         return self.filter(company=company) # Apply filter
+
+    def active(self, company):      
+        return self.filter( 
+                            company=company, # enforce tenant scoping
+                            is_active=True   # only fetch active records
+                        ) 
     # Enables query: 
-    # Invoice.objects.for_company(request.user.default_company)
+    # Invoice.objects.active(request.company)
 
 # Attach TenantQuerySet to .objects
 class TenantManager(models.Manager): 
@@ -18,5 +24,8 @@ class TenantManager(models.Manager):
     def for_company(self, company): # can call for_company() directly on objects
         return self.get_queryset().for_company(company)
     
+    def active(self, company):
+        return self.get_queryset().active(company)
+    
     # every model using TenantManager can call:
-    # Invoice.objects.for_company(request.user.default_company)
+    # Invoice.objects.active(request.company)
